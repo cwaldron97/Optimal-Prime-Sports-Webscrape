4f5496efd96e65d11d73d16d88fb88fdce051b42
henry-bulluck <thbulluck@gmail.com>
2019-03-03 16:22:07 -0600

    Rewrote code to use dictionaries and be more effective

--- Reading_Fantasy_Football_Example_Stats.py
+++ Reading_Fantasy_Football_Example_Stats.py
@@ -7,30 +7,42 @@ from openpyxl import Workbook, load_workbook
 #opens the excel file
 wb = load_workbook('Fantasy_Football_Example_stats_to_work_with.xlsx')
 
+positions = {} #positions 
+
 #loops through worksheets (ws)
 for ws in wb:
 
-    #prints the name of the worksheet
-    print(ws.title)
-
-    statName = []
+    #saves the name of the worksheet
+    positionName = ws.title
     
+    playerList = []
     #saves the first row of column names from the worksheet
     statNamesRow = ws[1]
 
     #saves the row as a list
+    statName = []
     for cell in statNamesRow:
         statName.append(cell.value)
     
     #itterates through rows 2 and down and takes their values
     for row in ws.iter_rows(min_row=2,values_only=True):
         #saves the row as a list of values
-        playerStats = []
-        for cell in row:
-            playerStats.append(cell)
-        
-        #prints the name of each value type and then the value itself
-        for index, val in enumerate(playerStats):
-            print(statName[index],val)
+        playerStats = {}
 
-    
+        #saves a player's stats as a dictionary
+        index = 0
+        for cell in row:
+            playerStats[statName[index]] = cell
+            index += 1
+
+        #saves list of player dictionaries
+        playerList.append(playerStats)
+    #saves the list of player dictionaries to their position
+    positions[positionName] = playerList
+
+#prints each player and their stats for each position
+for key in positions:
+    print(key)
+    for player in positions[key]:
+        print(player)
+          
