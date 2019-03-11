53139f293649aee3568e3381b009bc7b0a2cae7e
henry-bulluck <thbulluck@gmail.com>
2019-03-10 19:18:45 -0500

    Rewrote to save data in a new formate and made code more effective

--- Reading_Fantasy_Football_Example_Stats.py
+++ Reading_Fantasy_Football_Example_Stats.py
@@ -4,45 +4,31 @@
 
 from openpyxl import Workbook, load_workbook
 
-#opens the excel file
-wb = load_workbook('Fantasy_Football_Example_stats_to_work_with.xlsx')
+def readStatFile():
+    #opens the excel file
+    wb = load_workbook('Fantasy_Football_Example_stats_to_work_with.xlsx')
 
-positions = {} #positions 
+    playerList = {}
 
-#loops through worksheets (ws)
-for ws in wb:
+    #loops through worksheets (ws)
+    for ws in wb:
 
-    #saves the name of the worksheet
-    positionName = ws.title
-    
-    playerList = []
-    #saves the first row of column names from the worksheet
-    statNamesRow = ws[1]
-
-    #saves the row as a list
-    statName = []
-    for cell in statNamesRow:
-        statName.append(cell.value)
-    
-    #itterates through rows 2 and down and takes their values
-    for row in ws.iter_rows(min_row=2,values_only=True):
-        #saves the row as a list of values
-        playerStats = {}
-
-        #saves a player's stats as a dictionary
-        index = 0
-        for cell in row:
-            playerStats[statName[index]] = cell
-            index += 1
-
-        #saves list of player dictionaries
-        playerList.append(playerStats)
-    #saves the list of player dictionaries to their position
-    positions[positionName] = playerList
-
-#prints each player and their stats for each position
-for key in positions:
-    print(key)
-    for player in positions[key]:
-        print(player)
-          
+        #saves the name of the worksheet
+        positionName = ws.title
+        
+        #saves the first row of column names from the worksheet
+        statNames = ws[1]
+        
+        #itterates through rows 2 and down and takes their values
+        for row in ws.iter_rows(min_row=2):
+            #saves the player name
+            playerName = row[0].value
+            #saves the players position
+            playerStats = {"Postition": positionName}
+            #saves the players other stats
+            for index in range(1,len(row)):
+                playerStats[ statNames[index].value ] = row[index].value
+            #saves the players stats in playerList
+            playerList[playerName] = playerStats
+            
+    return playerList
