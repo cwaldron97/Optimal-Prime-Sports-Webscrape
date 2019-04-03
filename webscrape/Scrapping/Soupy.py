6ab2ad244942ca4583ee4343b32e210257acb713
cwaldron97 <32018000+cwaldron97@users.noreply.github.com>
2019-04-03 09:18:43 -0500

    Need to still fix module not found error

--- webscrape/Scrapping/Soupy.py
+++ webscrape/Scrapping/Soupy.py
@@ -1,12 +1,31 @@
+from requests import get
 from bs4 import BeautifulSoup
 
-from Scrapping.Request import response
 
+url = 'https://www.fantasypros.com/nfl/reports/leaders/qb.php?year=2018'
+response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 type(html_soup)
 Players = html_soup.find_all('tr')
-player = Players[0]
-player_name = player.td.find('td', class_='player-label')
-player_team = player.td.find('td', class_='center')
-player_team = player_team.text
+for x in range(0, 500):
+    player = Players[x]
+    statistics = player.find_all('td')
+    player_name = player.td.find('td', class_='player-label')
+    #insert function to pass player name to database
+    player_team = statistics.td.find('a', class_='center')
+    player_team = player_team.text
+    #insert function to pass player team to database
+    playerpositiontemp = statistics[4]
+    playerposition = playerpositiontemp.text
+    #insert function to pass player position to database
+    playerpointstemp = statistics[5]
+    playerpoints = playerpositiontemp.text
+    #insert function to pass player points to database
+    playergamestemp = statistics[6]
+    playergames = playergamestemp.text
+    #insert function to pass player games to database
+
+
+
+
 
